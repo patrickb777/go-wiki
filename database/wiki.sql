cf3e5780bd9a54ed1bfbd39ef03541144f14cc0b
Patrick B <patrick777@tuta.io>
2023-09-28 11:37:14 +0100

    Database schema changes

--- database/wiki.sql
+++ database/wiki.sql
@@ -1,30 +1,30 @@
-CREATE TABLE IF NOT EXISTS author (
-  authorID      SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS user (
+  userID        SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
   realName      TINYTEXT NOT NULL,
   userName      TINYTEXT NOT NULL,
   createdDate   DATETIME NOT NULL
 ) ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS articles (
-  articleID     SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS pages (
+  pageID        SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
   title         TINYTEXT NOT NULL,
-  body      TEXT,
+  body          TEXT,
   createdDate   DATETIME NOT NULL,
   updatedDate   DATETIME NOT NULL,
-  authorID      SMALLINT UNSIGNED NOT NULL,
-  CONSTRAINT `fk_article_author`
-    FOREIGN KEY (authorID) REFERENCES author (authorID)
+  userID      SMALLINT UNSIGNED NOT NULL,
+  CONSTRAINT `fk_page_user`
+    FOREIGN KEY (userID) REFERENCES user (userID)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT
 ) ENGINE = InnoDB;
 
-INSERT INTO author
+INSERT INTO user
   (realName, userName, createdDate)
 VALUES
   ('System', 'system', NOW());
 
-INSERT INTO articles
-  (title, body, createdDate, updatedDate, authorID)
+INSERT INTO pages
+  (title, body, createdDate, updatedDate, userID)
 VALUES
   ('Lorem Ipsum', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dictum, arcu eget consectetur interdum, risus magna viverra nibh, at placerat lacus est in dui. Aenean quis nisi id justo laoreet aliquam. Nunc a lectus interdum, rutrum massa quis, commodo nisl. Vestibulum ullamcorper, nunc id rutrum tempus, mi urna accumsan ex, ac faucibus elit lorem dictum nulla. Etiam dapibus, nulla non consectetur imperdiet, lacus lacus viverra lacus, dignissim gravida nulla odio eget libero. Suspendisse ac ex fermentum, vestibulum lorem eget, porta sapien. Vestibulum tempor enim vel elit egestas, eu hendrerit libero imperdiet. ', NOW(), NOW(), LAST_INSERT_ID());
 
--- wiki.go
+++ wiki.go
@@ -7,9 +7,12 @@ import (
 	"net/http"
 	"time"
 
+	// Import the database driver that's used for MariaDB
 	"github.com/go-sql-driver/mysql"
 )
 
+// Defining global variables in this block.  The GO DB tutorial doesn't recommend declaring the DB as global.
+// It suggests passing it through functions, however, the http Handler functions don't allow for that so for ease it's defined as global.
 var (
 	db   *sql.DB
 	tmpl *template.Template
@@ -21,18 +24,19 @@ type Page struct {
 	Body        []byte
 	CreatedDate time.Time
 	UpdatedDate time.Time
-	AuthorID    int64
+	UserID      int64
+	AuthorName  string
 }
 
 type User struct {
-	AuthorID       int64
+	UserID         int64
 	AuthorUserName string
 	AuthorName     string
 	CreatedDate    string
 }
 
-// You can only pass one value for templating
-// However, that can be a value of values, all datapoints wrapped into a Struct of Structs
+// You can only pass one value through templating.  Some of the http pages created use data from multiple queries.
+// As a workaround the results from each of these are included in a 'Payload' struct of structs which are reference via the Go templating.
 type Payload struct {
 	RecentList []Page
 	ViewPage   Page
@@ -42,15 +46,122 @@ func main() {
 	// Create the DB connection
 	dbCXN()
 
-	//Parse all html files
+	// To ensure that styling and images are served a http fileserver for static content is defined
+	http.Handle("/assets/", http.FileServer(http.Dir(".")))
+
+	// Parse all .html files into the templating
 	var err error
 	tmpl, err = tmpl.ParseGlob("html/*.html")
 	if err != nil {
 		log.Println(err)
 	}
-	http.HandleFunc("/", homeHandler)
-	http.Handle("/assets/", http.FileServer(http.Dir("."))) //Serve static content, i.e. image and css
 
+	// Handle root http requests
+	http.HandleFunc("/", homeHandler)
+
+	// Output some basic log information to console
+	log.Println("Application being served at http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func dbCXN() {
+	// Database connection properties
+	cfg := mysql.Config{
+		User:                 "app",
+		Passwd:               "jVqb2aren2Gm", //Hardcoded for testing
+		Net:                  "tcp",
+		Addr:                 "172.17.0.2:3306",
+		DBName:               "wiki",
+		ParseTime:            true,
+		AllowNativePasswords: true, // Stackoverflow fix for connection issue
+	}
+
+	// Get the database handle
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// ping the database to ensure connectivity
+	pingErr := db.Ping()
+	if err != nil {
+		log.Fatal(pingErr)
+	}
+	log.Println("Database is reachable!")
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	p := Payload{recentPages(), loadPage(1)}
+	tmpl.ExecuteTemplate(w, "index.html", p)
+}
+
+func createPage(p Page) int64 {
+	result, err := db.Exec("INSERT INTO pages (title, body, createdDate, updatedDate, userID) VALUES (?, ?, ?, ?, ?)", p.Title, p.Body, p.CreatedDate, p.UpdatedDate, p.UserID)
+	if err != nil {
+		log.Println(err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		log.Println(err)
+	}
+
+	return id
+}
+
+func loadPage(id int64) Page {
+	var p Page
+	query := "SELECT p.*, u.realName from pages p INNER JOIN user u on p.userID = u.userID where p.pageID = ?;"
+
+	row := db.QueryRow(query, id)
+	if err := row.Scan(&p.PageID, &p.Title, &p.Body, &p.CreatedDate, &p.UpdatedDate, &p.UserID, &p.AuthorName); err != nil {
+		log.Println(err)
+	}
+	log.Printf("Article '%s' loaded! \n", p.Title)
+
+	return p
+}
+
+func editPage(p Page) sql.Result {
+	result, err := db.Exec("UPDATE pages SET title = ?, body = ?, updatedDate = ? WHERE pageID = ?;", p.Title, p.Body, p.UpdatedDate, p.PageID)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return result
+}
+
+func recentPages() []Page {
+	var pages []Page
+
+	// Get the 10 most recent wiki pages
+	rows, err := db.Query("SELECT * FROM pages ORDER BY createdDate DESC LIMIT 10;")
+	if err != nil {
+		log.Println(err)
+	}
+	defer rows.Close()
+	// Populate the Page struct with query results
+	for rows.Next() {
+		var p Page
+		if err := rows.Scan(&p.PageID, &p.Title, &p.Body, &p.CreatedDate, &p.UpdatedDate, &p.UserID); err != nil {
+			log.Println(err)
+		}
+		pages = append(pages, p)
+	}
+
+	return pages
+}
+
+func recentUsers() {
+	// Returns the 10 most recent page IDs & Titles
+
+}
+
+func addUser() {
+	// Add user code here
+}
+
+func dbTests() {
 	// // Test page load function
 	// loadPage(1)
 
@@ -60,7 +171,7 @@ func main() {
 	// 	Body:        []byte("Sed nec bibendum quam. Nulla auctor euismod sapien, at congue orci tincidunt nec. Sed a tortor pretium, ornare nisl nec, volutpat lacus. Etiam tincidunt nulla ligula, id pretium felis vulputate et. Suspendisse potenti. Nunc non metus eu felis semper pellentesque in dictum magna. Proin quis dignissim eros, vitae interdum lectus. Aenean tempor at dolor quis lacinia. Mauris aliquam massa et lacus dapibus convallis. Donec eget est libero."),
 	// 	CreatedDate: time.Now(),
 	// 	UpdatedDate: time.Now(),
-	// 	AuthorID:    1,
+	// 	userID:    1,
 	// })
 	// log.Printf("Page ID '%v' created!", new)
 
@@ -79,101 +190,4 @@ func main() {
 	// 	fmt.Printf("%v, %s \n", p.PageID, p.Title)
 	// }
 	// serve http on port 8080
-	log.Println("Application being served at http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func dbCXN() {
-	// Database connection properties
-	cfg := mysql.Config{
-		User:                 "app",
-		Passwd:               "jVqb2aren2Gm", //Hardcoded for testing
-		Net:                  "tcp",
-		Addr:                 "172.17.0.2:3306",
-		DBName:               "wiki",
-		ParseTime:            true,
-		AllowNativePasswords: true, // Stackoverflow fix for connection issue
-	}
-
-	// Get database handle
-	var err error
-	db, err = sql.Open("mysql", cfg.FormatDSN())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	pingErr := db.Ping()
-	if err != nil {
-		log.Fatal(pingErr)
-	}
-	log.Println("Database is reachable!")
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	p := Payload{recentPages(), loadPage(1)}
-	tmpl.ExecuteTemplate(w, "index.html", p)
-}
-
-func createPage(p Page) int64 {
-	result, err := db.Exec("INSERT INTO articles (title, body, createdDate, updatedDate, authorID) VALUES (?, ?, ?, ?, ?)", p.Title, p.Body, p.CreatedDate, p.UpdatedDate, p.AuthorID)
-	if err != nil {
-		log.Println(err)
-	}
-	id, err := result.LastInsertId()
-	if err != nil {
-		log.Println(err)
-	}
-
-	return id
-}
-
-func loadPage(id int64) Page {
-	var p Page
-
-	row := db.QueryRow("SELECT * FROM articles WHERE articleID = 1;")
-	if err := row.Scan(&p.PageID, &p.Title, &p.Body, &p.CreatedDate, &p.UpdatedDate, &p.AuthorID); err != nil {
-		log.Println(err)
-	}
-	log.Printf("Article '%s' loaded! \n", p.Title)
-
-	return p
-}
-
-func editPage(p Page) sql.Result {
-	result, err := db.Exec("UPDATE articles SET title = ?, body = ?, updatedDate = ? WHERE articleID = ?;", p.Title, p.Body, p.UpdatedDate, p.PageID)
-	if err != nil {
-		log.Println(err)
-	}
-
-	return result
-}
-
-func recentPages() []Page {
-	var pages []Page
-
-	// Get the 10 most recent wiki pages
-	rows, err := db.Query("SELECT * FROM articles ORDER BY createdDate DESC LIMIT 10;")
-	if err != nil {
-		log.Println(err)
-	}
-	defer rows.Close()
-	// Populate the Page struct with query results
-	for rows.Next() {
-		var p Page
-		if err := rows.Scan(&p.PageID, &p.Title, &p.Body, &p.CreatedDate, &p.UpdatedDate, &p.AuthorID); err != nil {
-			log.Println(err)
-		}
-		pages = append(pages, p)
-	}
-
-	return pages
-}
-
-func recentUsers() {
-	// Returns the 10 most recent page IDs & Titles
-
-}
-
-func addUser() {
-	// Add user code here
 }
